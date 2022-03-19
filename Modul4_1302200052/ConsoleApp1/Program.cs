3349842f48c3e58cdb811a75e250c33be59c8087
glenrioaristo <glenrioaristo@student.telkomuniversity.ac.id>
2022-03-19 16:45:08 +0700

    menambahkan class Penjumlahan dengan methodJumlahTigaAngka

--- Modul4_1302200052/ConsoleApp1/Program.cs
+++ Modul4_1302200052/ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ namespace Modul4_1302200052
     {
         public static void Main(string[] args)
         {
-            // NIM = 1302204051
+            // NIM = 1302200052
             Penjumlahan.JumlahTigaAngka<float>(13, 02, 20);
 
         }
